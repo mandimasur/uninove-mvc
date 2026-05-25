3c9c841d5c83ccf5b168b28937cee59607bacc9f
mandimasur <mandimasur@gmail.com>
2026-05-25 22:33:03 +0000

    Criação Mvc

---
+++ uninove.web/Controllers/AlunoController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class AlunoController : Controller
+{
+    public IActionResult Index ()
+    {
+        ViewBag.Nome = "Amanda";
+        ViewBag.Curso = "ADS";
+        ViewBag.Semestre = "1ºSemestre";
+        
+        return View();
+
+    }
+
+    public IActionResult Detalhes (int id)
+    {
+        ViewBag.AlunoID = id;
+        retorn View();
+    
+
+    }
+}
